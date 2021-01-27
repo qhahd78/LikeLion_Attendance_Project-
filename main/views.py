292e2b3f6354f7334373354d1d503ed07256fead
qhahd78 <umuj0708@likelion.org>
2021-01-27 20:50:14 +0900

    끝.

--- main/views.py
+++ main/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
 from .models import Student
 from django.contrib.auth.decorators import login_required
-import json
 # Create your views here.
 
 @login_required(login_url='login')
 def home(request) : 
     students= Student.objects
     return render (request, 'main.html', {'students': students})
-
-def name_list(request):
-    N_list = Student.objects.all()
-    context = {
-        "names": names,
-        "name_js": json.dumps([name_li.json() for name_li in names])
-    }
-    return render(request, "main.html", context)
